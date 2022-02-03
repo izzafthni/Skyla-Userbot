f3f0e3ccffc54f49b8d387e5c14b7ee18d1b8d65
SkylaIND <skylabot0@gmail.com>
2022-02-03 08:44:16 +0000

    pylint: auto fixes
    Signed-off-by: SkylaIND <skylabot0@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -200,7 +200,9 @@ YOUTUBE_API_KEY = os.environ.get(
     "YOUTUBE_API_KEY") or "AIzaSyACwFrVv-mlhICIOCvDQgaabo6RIoaK8Dg"
 
 # Untuk Perintah .kayalive
-SKYLA_TEKS_KUSTOM = os.environ.get("SKYLA_TEKS_KUSTOM", "I'am Using Skyla-Userbot⚡")
+SKYLA_TEKS_KUSTOM = os.environ.get(
+    "SKYLA_TEKS_KUSTOM",
+    "I'am Using Skyla-Userbot⚡")
 
 # Untuk Mengubah Pesan Welcome
 START_WELCOME = os.environ.get("START_WELCOME", None)
