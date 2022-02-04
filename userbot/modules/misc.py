73268cbb2fa38203eee04970d8d793c3092fefdb
Skyla IND <98250309+SkylaIND@users.noreply.github.com>
2022-02-04 12:50:58 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -85,7 +85,8 @@ async def killdabot(event):
 
 @register(outgoing=True, pattern="^.readme$")
 async def reedme(e):
-    await e.edit("┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+    await e.edit(
+                 "┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                  "Here's Something for You to Read :\n"
                  "\n[Skyla-Userbot Repo](https://github.com/SkylaIND/Skyla-Userbot/blob/Skyla-Userbot/README.md)"
                  "\n[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-11-02)"
