855a2087e6c1605480e196daec98b06a187a789e
Skyla IND <98250309+SkylaIND@users.noreply.github.com>
2022-02-15 23:48:23 +0700

    Update format.py

--- userbot/utils/format.py
+++ userbot/utils/format.py
@@ -2,6 +2,7 @@
 import datetime
 
 from bs4 import BeautifulSoup
+from markdown import markdown
 from telethon.tl.tlobject import TLObject
 from telethon.tl.types import MessageEntityPre
 from telethon.utils import add_surrogate
