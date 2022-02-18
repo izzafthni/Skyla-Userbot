dbb7c29bbcaeed991d0b60e7c3a87c1f7c70588b
Skyla IND <98250309+SkylaIND@users.noreply.github.com>
2022-02-18 09:32:56 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -405,7 +405,7 @@ with bot:
         bot.loop.run_until_complete(checking())
     except BaseException:
         LOGS.info(
-            "Join Support Group @NastySupportt and Channel @NastyProject to see the updates of userbot"
+            "Join Support Group @skylasupport and Channel @SkylaIND to see the updates of userbot"
             "Don't Leave")
         quit(1)
 
