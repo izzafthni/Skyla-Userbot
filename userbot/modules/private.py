6c0d7501cdda8cae8e5f139bc6b4d86329177266
Skyla IND <98250309+SkylaIND@users.noreply.github.com>
2022-02-22 09:03:40 +0700

    Update private.py

--- userbot/modules/private.py
+++ userbot/modules/private.py
@@ -505,16 +505,14 @@ async def bot_start(event):
                         \n**Anda dapat Menghubungi [{OWNER}](tg://user?id={OWNER_ID}) dari sini.**\
                         \n**Jangan Melakukan Spam Atau anda akan di blokir**\
                         \n\n**Powered by** [Skyla-Userbot](https://github.com/SkylaIND/Skyla-Userbot)"
-        buttons = [
-            (
+        buttons=[
+            [
                 Button.url("ɢʀᴏᴜᴘ", f"https://t.me/skylasupport"),
-                Button.url(
-                    "ᴄʜᴀɴɴᴇʟ",
-                    f"https://t.me/SkylaIND",
-                (
-                    Button.inline("ɪɴꜰᴏ", data="infor"),
-                )
-            ]
+                Button.url("ᴄʜᴀɴɴᴇʟ", f"https://t.me/SkylaIND"),
+            ],
+            [Button.inline("ɪɴꜰᴏ", data="infor")],
+        ],
+    )
     else:
         start_msg=f"**Menu ini Hanya Terlihat Oleh [{OWNER}](tg://user?id={OWNER_ID})** ..!"
         buttons=[
