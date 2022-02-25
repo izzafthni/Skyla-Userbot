7478cd188cc82dac7016764a7bd0f6f40c31d112
Skyla IND <98250309+SkylaIND@users.noreply.github.com>
2022-02-25 17:08:14 +0700

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -12,7 +12,7 @@ async def startupmessage():
             await tgbot.send_file(
                 BOTLOG_CHATID,
                 "https://telegra.ph/file/0970dd27e9141599aaccf.jpg",
-                caption="⚡ **Skyla-Userbot berhasil diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @SkylaIND ",
+                caption="⚡ **Skyla-Userbot berhasil diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@Skyla-Userbot\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @SkylaIND ",
                 buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/skylasupport"),)],
             )
     except Exception as e:
