df7ef4b723e513cbfc7890385f701c2344bb75f1
SkylaIND <skylabot0@gmail.com>
2022-03-05 09:57:07 +0000

    pylint: auto fixes
    Signed-off-by: SkylaIND <skylabot0@gmail.com>

--- userbot/modules/converter.py
+++ userbot/modules/converter.py
@@ -16,7 +16,7 @@ from PIL import Image
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import edit_delete, edit_or_reply, skyla_cmd, runcmd
+from userbot.utils import edit_delete, edit_or_reply, runcmd
 
 
 @kyy_cmd(pattern="convert ?(foto|audio|gif|voice|photo|mp3)? ?(.*)")
