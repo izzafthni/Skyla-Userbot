2289c7b096cee44f2b3d8317c225d80fcb932213
Skyla IND <98250309+SkylaIND@users.noreply.github.com>
2022-03-06 13:16:10 +0700

    Update vcg.py

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -1,3 +1,21 @@
+Skip to content
+muhammadrizky16
+/
+Kyy-Userbot
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Kyy-Userbot/userbot/modules/vcg.py
+@muhammadrizky16
+muhammadrizky16 Kyy-Userbot v3.1.0 updates add sudo modules, fixes convert
+ 1 contributor
+124 lines (99 sloc)  3.81 KB
 # Thanks Full To Team Ultroid
 # Fiks By Kyy @IDnyaKosong
 
@@ -8,16 +26,27 @@ from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
 from telethon.tl.functions.phone import GetGroupCallRequest as getvc
 from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
 
-from userbot import CMD_HELP
+from telethon.tl import types
+from telethon.utils import get_display_name
+
+from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import edit_delete, edit_or_reply, skyla_cmd
 from userbot.events import register
 
 NO_ADMIN = "`Maaf Kamu Bukan Admin 👮`"
 
 
-async def get_call(event):
-    kyy = await event.client(getchat(event.chat_id))
-    kyy = await event.client(getvc(kyy.full_chat.call, limit=1))
-    return kyy.call
+def vcmention(user):
+    full_name = get_display_name(user)
+    if not isinstance(user, types.User):
+        return full_name
+    return f"[{full_name}](tg://user?id={user.id})"
+
+
+async def get_call(kyy):
+    kyy = await kyy.client(getchat(kyy.chat_id))
+    await kyy.client(getvc(kyy.full_chat.call, limit=1))
+    return hehe.call
 
 
 def user_list(l, n):
@@ -25,41 +54,43 @@ def user_list(l, n):
         yield l[i: i + n]
 
 
-@register(outgoing=True, pattern=r"^\.startvc$")
+@skyla_cmd(pattern="startvc$")
+@register(pattern=r"^\.startvcs$", sudo=True)
 async def start_voice(c):
     chat = await c.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
-        await c.edit(f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
+        await edit_delete(c, f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
         return
     try:
         await c.client(startvc(c.chat_id))
-        await c.edit("`Memulai Obrolan Suara`")
+        await edit_or_reply(c, "`Memulai Obrolan Suara`")
     except Exception as ex:
-        await c.edit(f"**ERROR:** `{ex}`")
+        await edit_or_reply(c, f"**ERROR:** `{ex}`")
 
 
-@register(outgoing=True, pattern=r"^\.stopvc$")
+@skyla_cmd(pattern="stopvc$")
+@register(pattern=r"^\.stopvcs$", sudo=True)
 async def stop_voice(c):
     chat = await c.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
-        await c.edit(f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
+        await edit_delete(c, f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
         return
     try:
         await c.client(stopvc(await get_call(c)))
-        await c.edit("`Mematikan Obrolan Suara`")
+        await edit_or_reply(c, "`Mematikan Obrolan Suara`")
     except Exception as ex:
-        await c.edit(f"**ERROR:** `{ex}`")
+        await edit_delete(c, f"**ERROR:** `{ex}`")
 
 
-@register(outgoing=True, pattern=r"^\.vcinvite", groups_only=True)
+@skyla_cmd(pattern="vcinvite")
 async def _(kyy):
-    await kyy.edit("`Sedang Menginvite Member...`")
+    await edit_or_reply(kyy, "`Sedang Menginvite Member...`")
     users = []
     z = 0
     async for x in kyy.client.iter_participants(kyy.chat_id):
@@ -72,16 +103,40 @@ async def _(kyy):
             z += 6
         except BaseException:
             pass
-    await kyy.edit(f"`Menginvite {z} Member`")
+    await edit_or_reply(kyy, f"`Menginvite {z} Member`")
+
+
+@skyla_cmd(pattern="vctitle(?: |$)(.*)")
+@register(pattern=r"^\.cvctitle$", sudo=True)
+async def change_title(e):
+    title = e.pattern_match.group(1)
+    me = await e.client.get_me()
+    chat = await e.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not title:
+        return await edit_delete(e, "**Silahkan Masukan Title Obrolan Suara Grup**")
+
+    if not admin and not creator:
+        await edit_delete(e, f"**Maaf {me.first_name} Bukan Admin 👮**")
+        return
+    try:
+        await e.client(settitle(call=await get_call(e), title=title.strip()))
+        await edit_or_reply(e, f"**Berhasil Mengubah Judul VCG Menjadi** `{title}`")
+    except Exception as ex:
+        await edit_delete(e, f"**ERROR:** `{ex}`")
 
 
 CMD_HELP.update(
     {
-        "vcg": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.startvc`\
+        "vcg": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}startvc`\
          \n↳ : Memulai Obrolan Suara dalam Group.\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.stopvc`\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}stopvc`\
          \n↳ : `Menghentikan Obrolan Suara Pada Group.`\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.vcinvite`\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}vctittle <tittle vcg>`\
+         \n↳ : `Mengubah tittle/judul Obrolan Suara.`\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}vcinvite`\
          \n↳ : Invite semua member yang berada di group."
     }
 )
